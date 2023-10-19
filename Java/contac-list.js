f978c2d3cd7aefaf844bcecbb81f03a72d328e69
Manuel Aguilar <manueleaguilarr@gmail.com>
2023-10-19 12:14:50 -0600

    primera practica de Javascript

---
+++ Java/contac-list.js
@@ -0,0 +1,38 @@
+let listaContacto = [
+    {
+    id : 1,
+    nombre: "Manuel",
+    apellido: "Aguilar",
+    celular: "98102595",
+    ubicacion: {ciudad: "Choluteca"}
+},
+
+{
+    id : 2,
+    nombre: "Jose",
+    apellido: "Perez",
+    celular: "999999",
+    ubicacion: {ciudad: "Cortes"}
+},
+
+];
+
+function agregarContacto(nuevoContacto){
+    listaContacto.push(nuevoContacto);
+}
+
+function contactoBorrar(borrarContacto){
+    let nuevalista = listaContacto.filter((contacto)=>contacto.id !== contactoBorrar);
+}
+
+function imprimirContacto(){
+    console.log(listaContacto);
+}
+
+let nuevoContacto = {
+    id: 2,
+    nombre: "Carlos",
+    apellido: "Paz",
+    celular: "88888888",
+    ubicacion: {ciudad: "San Pedro Sula",}
+}
---
+++ Java/practicaJava.js
@@ -0,0 +1,6 @@
+let numero1 = 1
+let numero2 = 2
+
+if(numero1 < numero2) {
+    console.log("el numero mayor es" + numero2)
+}
